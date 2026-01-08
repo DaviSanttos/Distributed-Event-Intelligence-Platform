8a715d1f79f514d38b4f26ce6759ced10af58244
DaviSanttos <davicelso.santos@hotmail.com>
2026-01-08 19:30:55 -0300

    ft: add initial structure

---
+++ src/main/java/dev/DistributedEventIntelligencePlatformApplication/DistributedEventIntelligencePlatformApplication.java
@@ -0,0 +1,13 @@
+package dev.DistributedEventIntelligencePlatformApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedEventIntelligencePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedEventIntelligencePlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/DistributedEventIntelligencePlatformApplication/DistributedEventIntelligencePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.DistributedEventIntelligencePlatformApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedEventIntelligencePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
